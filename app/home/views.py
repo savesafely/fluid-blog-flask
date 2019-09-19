6f06b50b34d4b3594588d9aaa33182d58d272a93
913648653 <juukee>
2019-09-19 12:11:07 +0800

    change name

--- app/home/views.py
+++ app/home/views.py
@@ -7,6 +7,18 @@ from flask import render_template
 def index():
     return render_template('home/index.html')
 
+@home.route('/archives')
+def archives():
+    return render_template('home/archives.html')
+    
+@home.route('/tags')
+def tags():
+    return render_template('home/tags.html')   
+    
+@home.route('/about')
+def about():
+    return render_template('home/about.html')   
+
 @home.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
