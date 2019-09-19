a0036a4d73f57ffbf30acfa091eff936508d27d8
913648653 <juukee>
2019-09-19 18:43:26 +0800

    生成静态页面 001

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://blog.db/'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.model):
+  id = db.Column(db.Integer, primary_key=True)
+  
