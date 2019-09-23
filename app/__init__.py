0d09322cd70ce5f93034945ba359ccaba6f98bcf
wm.xu-618 <913648653@qq.com>
2019-09-23 13:55:17 +0800

    '增加config目录'

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@
 from flask import Flask
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
-
+from . import config
 
 app = Flask(__name__)
-app.debug = True
+app.config.from_object(config)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://blog.db/'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
