7064a2c6eef009183b5fc4e0df866a683ea5fdf7
juukee <az913648653@163.com>
2019-09-23 10:18:05 +0800

    生成静态页面 001 (#3)

--- app/home/views.py
+++ app/home/views.py
@@ -1,11 +1,20 @@
 # -*- coding:utf-8 -*-
 from . import home
+from flask import Flask
 from flask import render_template
+import os
+app = Flask(__name__)
 
 @home.route('/')
 @home.route('/index')
 def index():
+    filename = os.path.join(app.root_path, '../public','index.html')
+    print(filename)
+    with open(filename,'w') as f:
+        f.write(str(render_template('home/index.html')))
+        f.close()
     return render_template('home/index.html')
+      
 
 @home.route('/archives')
 def archives():
