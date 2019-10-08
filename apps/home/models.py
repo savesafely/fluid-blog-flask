b62c06b3a194428f74b5465d6a2bf62fafb7d0b2
913648653 <juukee>
2019-10-08 22:56:00 +0800

    增加主题切换功能

--- apps/home/models.py
+++ apps/home/models.py
@@ -6,7 +6,7 @@ class BlogModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100),nullable=False)
     content = db.Column(db.Text(1000),nullable=False)
-    create_time = db.Column(db.DATETIME,default=datetime.utcnow())
+    create_time = db.Column(db.DateTime,default=datetime.utcnow())
 
     def __init__(self, title, content):
         self.title = title
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 DEBUG = True
 
 #数据库连接
@@ -10,3 +12,11 @@ PER_PAGE = 10
 
 #SERVER_NAME = ''
 
+DEFAULT_THEME = 'default'
+
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))   
+STATIC = os.path.join(APP_ROOT, 'static')
+TEMPLATE = os.path.join(APP_ROOT, 'templates')
+PUBLIC = os.path.join(APP_ROOT, 'public')
