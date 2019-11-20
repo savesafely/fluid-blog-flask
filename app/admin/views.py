a9fb016d74b10ca368649320ad7ba0142d125853
root <root@localhost.localdomain>
2019-11-20 22:11:50 +0800

    g

--- app/admin/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from . import admin
-from flask import render_template
-def view(html):
-    return render_template('admin/' + html)
-
-@admin.route('/')
-def index():
-    return view('index.html')
